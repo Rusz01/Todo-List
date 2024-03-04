6104d32e25817ed334dedc8f16acc10f19019985
Rusz01 <ruszbaidhya@gmail.com>
2024-03-04 10:02:57 +0000

    Saving task and description into database

--- home/models.py
+++ home/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-    title = models.CharField(max_length=30, default='')
-    desc = models.TextField(default='')
+    title = models.CharField(max_length=30)
+    desc = models.TextField()
     time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
